9e44c6e50d9d30d083baeb5deabaea45837db8d5
Didy Septiyono <didy@didy.project>
2018-08-03 06:53:09 +0700

    make it better

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -59,19 +59,19 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     }
 
     pixPaint.setFont(QFont(font, 28 * fontFactor));
-    fm = pixPaint.fontMetrics();
-    //titleTextWidth = fm.width(titleText);
-    pixPaint.drawText(paddingLeft, paddingTop, titleText);
-
-    pixPaint.setFont(QFont(font, 15 * fontFactor));
-    pixPaint.drawText(paddingLeft, paddingTop + titleVersionVSpace, versionText);
-
-    // draw copyright stuff
-    pixPaint.setFont(QFont(font, 10 * fontFactor));
-    pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace, copyrightTextBtc);
-    pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 12, copyrightTextDash);
-    pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 24, copyrightTextPIVX);
-    pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 36, copyrightTextDividendCash);
+    // fm = pixPaint.fontMetrics();
+    // //titleTextWidth = fm.width(titleText);
+    // pixPaint.drawText(paddingLeft, paddingTop, titleText);
+    //
+    // pixPaint.setFont(QFont(font, 15 * fontFactor));
+    // pixPaint.drawText(paddingLeft, paddingTop + titleVersionVSpace, versionText);
+    //
+    // // draw copyright stuff
+    // pixPaint.setFont(QFont(font, 10 * fontFactor));
+    // pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace, copyrightTextBtc);
+    // pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 12, copyrightTextDash);
+    // pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 24, copyrightTextPIVX);
+    // pixPaint.drawText(paddingLeft, paddingTop + titleCopyrightVSpace + 36, copyrightTextDividendCash);
 
     // draw additional text if special network
     if (!titleAddText.isEmpty()) {
